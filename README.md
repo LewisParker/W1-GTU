5403fc9482f28c31e92db44c9d6fa6eedf7300d6
LewisParker <surajpanday027@gmail.com>
2016-01-10 16:34:41 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # W1-GTU
 Created during the Workshop
+
---
+++ w1.py
@@ -0,0 +1,13 @@
+#IF else program
+a=1 #variable declaration
+if a==1:
+    print"Correct"
+else:
+    print"Incorrect"
+
+    #Function in python
+
+def printf():
+    print"Hello"
+
+printf()    
