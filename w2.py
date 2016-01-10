e533238ced48ee1f35e22b930fc7bf32828b6af3
LewisParker <surajpanday027@gmail.com>
2016-01-10 16:48:01 +0530

    This is second commit

---
+++ w2.py
@@ -0,0 +1,14 @@
+#program to find the largest number
+a=1
+b=10
+c=3
+if(a>b):
+    if a>c:
+        print"Number a=%d is greater"%a
+elif(b>c):
+    print"Number b=%d is greater"%b
+else:
+    print"Number c=%d is greater"%c
+print max(a,b,c)
+    
+    
